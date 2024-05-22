6d5da6595fedcdd073ec445c99db1317acd778fd
Dinis Cruz <dinis.cruz@owasp.org>
2024-05-22 16:01:32 +0100

    added support for creating the ECR repo when not existing

--- osbot_nginx/docker/nginx_in_lambda__mvp/Nginx_In_Lambda__MVP.py
+++ osbot_nginx/docker/nginx_in_lambda__mvp/Nginx_In_Lambda__MVP.py
@@ -8,7 +8,7 @@ ENV_VARS__REQUIRED = ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY',
 
 class Nginx_In_Lambda__MVP(Kwargs_To_Self):
     aws_config     : AWS_Config
-    container_name : str        = 'nginx_in_lambda__mvp'
+    container_name : str        = 'nginx-in-lambda_mvp'
 
 
     @cache_on_self
@@ -16,5 +16,18 @@ class Nginx_In_Lambda__MVP(Kwargs_To_Self):
         return ECR()
 
     def ecr_container(self):
-        return self.ecr().repositories()
-        #return self.container_name
+        return self.ecr().repository_info(self.container_name)
+
+    def ecr_container_arn(self):
+        region_name = self.aws_config.region_name()
+        account_id  = self.aws_config.account_id()
+        return f'arn:aws:ecr:{region_name}:{account_id}:repository/{self.container_name}'
+
+    def setup(self):
+        target_repo_info = self.ecr().repository_info(self.container_name)
+        if not target_repo_info :
+            self.ecr().repository_create(self.container_name)
+            target_repo_info = self.ecr().repository_info(self.container_name)
+
+        setup_data = dict(target_repo_info=target_repo_info)
+        return setup_data
--- tests/integration/docker/nginx_in_lambda__mvp/test_Nginx_In_Lambda__MVP.py
+++ tests/integration/docker/nginx_in_lambda__mvp/test_Nginx_In_Lambda__MVP.py
@@ -1,6 +1,7 @@
 from os import getenv
 from unittest import TestCase
 
+from osbot_aws.aws.boto3.View_Boto3_Rest_Calls import print_boto3_calls
 from osbot_utils.utils.Dev import pprint
 from osbot_utils.utils.Misc import list_set
 
@@ -14,19 +15,22 @@ class test_Nginx_In_Lambda__MVP(TestCase):
     def setUpClass(cls):
         cls.nginx_in_lambda = Nginx_In_Lambda__MVP()
 
-    def test_setup(self):
-        assert type(self.nginx_in_lambda) is Nginx_In_Lambda__MVP
-
     def test_check_env_variables(self):
         for env_var_name in ENV_VARS__REQUIRED:
             assert getenv(env_var_name) is not None , f'Env var was not configured: {env_var_name}'
 
     def test_ecr_container(self):
-        containers = self.nginx_in_lambda.ecr_container()
-        assert len(containers) == 1
-        assert list_set(containers[0]) == ['createdAt', 'encryptionConfiguration', 'imageScanningConfiguration',
+        ecr_container = self.nginx_in_lambda.ecr_container()
+
+        assert list_set(ecr_container) == ['createdAt', 'encryptionConfiguration', 'imageScanningConfiguration',
                                            'imageTagMutability', 'registryId', 'repositoryArn', 'repositoryName',
                                            'repositoryUri']
 
-        #result = self.nginx_in_lambda.ecr_container()
-        #pprint(result)
+    def test_setup(self):
+        with self.nginx_in_lambda as _:
+            setup_data       = _.setup()
+            target_repo_info = setup_data.get('target_repo_info')
+            assert list_set(setup_data) == ['target_repo_info']
+            assert target_repo_info.get('repositoryName') == _.container_name
+
+            assert target_repo_info.get('repositoryArn' ) == _.ecr_container_arn()
