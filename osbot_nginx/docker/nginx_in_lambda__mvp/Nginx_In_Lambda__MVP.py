c41aaaeebebad33373d36a6557270799de8145dd
Dinis Cruz <dinis.cruz@owasp.org>
2024-05-22 16:14:09 +0100

    added test to confirm that we have access to all source_files in the GH action build

--- osbot_nginx/docker/nginx_in_lambda__mvp/Nginx_In_Lambda__MVP.py
+++ osbot_nginx/docker/nginx_in_lambda__mvp/Nginx_In_Lambda__MVP.py
@@ -2,14 +2,18 @@ from osbot_aws.AWS_Config import AWS_Config
 from osbot_aws.apis.ECR import ECR
 from osbot_utils.base_classes.Kwargs_To_Self import Kwargs_To_Self
 from osbot_utils.decorators.methods.cache_on_self import cache_on_self
+from osbot_utils.utils.Files import path_combine, files_list, files_recursive, file_name
+
+from osbot_nginx.docker import nginx_in_lambda__mvp
 
 ENV_VARS__REQUIRED = ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY',
                       'AWS_ACCOUNT_ID'   , 'AWS_DEFAULT_REGION'   ]
 
+FOLDER__NGINX_IN_LAMBDA__MVP__SOURCE_FILES = 'source_files'
+
 class Nginx_In_Lambda__MVP(Kwargs_To_Self):
     aws_config     : AWS_Config
     container_name : str        = 'nginx-in-lambda_mvp'
-
 
     @cache_on_self
     def ecr(self):
@@ -22,6 +26,16 @@ class Nginx_In_Lambda__MVP(Kwargs_To_Self):
         region_name = self.aws_config.region_name()
         account_id  = self.aws_config.account_id()
         return f'arn:aws:ecr:{region_name}:{account_id}:repository/{self.container_name}'
+
+    def files_in_source_files(self):
+        files = {}
+        for file_path in files_recursive(self.path_source_files()):
+            files[file_name(file_path)] = file_path
+        return files
+
+    def path_source_files(self):
+        return path_combine(nginx_in_lambda__mvp.path, FOLDER__NGINX_IN_LAMBDA__MVP__SOURCE_FILES)
+
 
     def setup(self):
         target_repo_info = self.ecr().repository_info(self.container_name)
--- tests/integration/docker/nginx_in_lambda__mvp/test_Nginx_In_Lambda__MVP.py
+++ tests/integration/docker/nginx_in_lambda__mvp/test_Nginx_In_Lambda__MVP.py
@@ -3,9 +3,11 @@ from unittest import TestCase
 
 from osbot_aws.aws.boto3.View_Boto3_Rest_Calls import print_boto3_calls
 from osbot_utils.utils.Dev import pprint
+from osbot_utils.utils.Files import folder_exists, folder_name, folder_files, files_names, files_list, file_exists
 from osbot_utils.utils.Misc import list_set
 
-from osbot_nginx.docker.nginx_in_lambda__mvp.Nginx_In_Lambda__MVP import Nginx_In_Lambda__MVP, ENV_VARS__REQUIRED
+from osbot_nginx.docker.nginx_in_lambda__mvp.Nginx_In_Lambda__MVP import Nginx_In_Lambda__MVP, ENV_VARS__REQUIRED, \
+    FOLDER__NGINX_IN_LAMBDA__MVP__SOURCE_FILES
 
 
 class test_Nginx_In_Lambda__MVP(TestCase):
@@ -25,6 +27,22 @@ class test_Nginx_In_Lambda__MVP(TestCase):
         assert list_set(ecr_container) == ['createdAt', 'encryptionConfiguration', 'imageScanningConfiguration',
                                            'imageTagMutability', 'registryId', 'repositoryArn', 'repositoryName',
                                            'repositoryUri']
+
+    def test_files_in_source_files(self):
+        expected_files = ['Dockerfile', 'build-and-publish-docker-image.sh', 'index.html', 'nginx.conf']
+        files          = self.nginx_in_lambda.files_in_source_files()
+
+        assert list_set(files) == expected_files
+        for file_path in files.values():
+            assert file_exists(file_path)
+
+    def test_path_source_files(self):
+        path_source_files = self.nginx_in_lambda.path_source_files()
+        files_in_parh     = files_list(path_source_files, pattern='*')
+        assert folder_exists(path_source_files)
+        assert folder_name  (path_source_files) == FOLDER__NGINX_IN_LAMBDA__MVP__SOURCE_FILES
+        assert 'Dockerfile' in files_names(files_list(path_source_files, pattern='*'))
+
 
     def test_setup(self):
         with self.nginx_in_lambda as _:
