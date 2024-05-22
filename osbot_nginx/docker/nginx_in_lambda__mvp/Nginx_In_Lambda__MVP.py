75c80c447a6d21644262e319c3b8fe46a89153e7
Dinis Cruz <dinis.cruz@owasp.org>
2024-05-22 17:12:52 +0100

    started adding support for using OSBot_AWS Create_Image_ECR util class

--- osbot_nginx/docker/nginx_in_lambda__mvp/Nginx_In_Lambda__MVP.py
+++ osbot_nginx/docker/nginx_in_lambda__mvp/Nginx_In_Lambda__MVP.py
@@ -1,31 +1,41 @@
 from osbot_aws.AWS_Config import AWS_Config
 from osbot_aws.apis.ECR import ECR
+from osbot_aws.helpers.Create_Image_ECR import Create_Image_ECR
 from osbot_utils.base_classes.Kwargs_To_Self import Kwargs_To_Self
 from osbot_utils.decorators.methods.cache_on_self import cache_on_self
 from osbot_utils.utils.Files import path_combine, files_list, files_recursive, file_name
 
+import osbot_nginx
 from osbot_nginx.docker import nginx_in_lambda__mvp
 
 ENV_VARS__REQUIRED = ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY',
                       'AWS_ACCOUNT_ID'   , 'AWS_DEFAULT_REGION'   ]
 
-FOLDER__NGINX_IN_LAMBDA__MVP__SOURCE_FILES = 'source_files'
+FOLDER__DOCKER__IMAGES_FILES = '../docker_files'
 
 class Nginx_In_Lambda__MVP(Kwargs_To_Self):
     aws_config     : AWS_Config
-    container_name : str        = 'nginx-in-lambda_mvp'
+    repository_name : str        = 'nginx-in-lambda_mvp'
 
     @cache_on_self
     def ecr(self):
         return ECR()
 
-    def ecr_container(self):
-        return self.ecr().repository_info(self.container_name)
+    def create_image_ecr(self):
+        return Create_Image_ECR(image_name=self.repository_name, path_images=self.path_docker_image_files())
 
-    def ecr_container_arn(self):
+    def ecr_repository(self):
+        return self.ecr().repository_info(self.repository_name)
+
+    def ecr_repository_arn(self):
+        region_name = self.aws_config.region_name()
+        account_id = self.aws_config.account_id()
+        return f'arn:aws:ecr:{region_name}:{account_id}:repository/{self.repository_name}'
+
+    def ecr_container_uri(self):
         region_name = self.aws_config.region_name()
         account_id  = self.aws_config.account_id()
-        return f'arn:aws:ecr:{region_name}:{account_id}:repository/{self.container_name}'
+        return f"{account_id}.dkr.ecr.{region_name}.amazonaws.com/{self.repository_name}"
 
     def files_in_source_files(self):
         files = {}
@@ -34,14 +44,16 @@ class Nginx_In_Lambda__MVP(Kwargs_To_Self):
         return files
 
     def path_source_files(self):
-        return path_combine(nginx_in_lambda__mvp.path, FOLDER__NGINX_IN_LAMBDA__MVP__SOURCE_FILES)
+        return path_combine(self.path_docker_image_files(), self.repository_name)
 
+    def path_docker_image_files(self):
+        return path_combine(osbot_nginx.path, FOLDER__DOCKER__IMAGES_FILES)
 
     def setup(self):
-        target_repo_info = self.ecr().repository_info(self.container_name)
+        target_repo_info = self.ecr().repository_info(self.repository_name)
         if not target_repo_info :
-            self.ecr().repository_create(self.container_name)
-            target_repo_info = self.ecr().repository_info(self.container_name)
+            self.ecr().repository_create(self.repository_name)
+            target_repo_info = self.ecr().repository_info(self.repository_name)
 
         setup_data = dict(target_repo_info=target_repo_info)
         return setup_data
--- tests/integration/docker/nginx_in_lambda__mvp/test_Nginx_In_Lambda__MVP.py
+++ tests/integration/docker/nginx_in_lambda__mvp/test_Nginx_In_Lambda__MVP.py
@@ -5,9 +5,9 @@ from osbot_aws.aws.boto3.View_Boto3_Rest_Calls import print_boto3_calls
 from osbot_utils.utils.Dev import pprint
 from osbot_utils.utils.Files import folder_exists, folder_name, folder_files, files_names, files_list, file_exists
 from osbot_utils.utils.Misc import list_set
+from osbot_utils.utils.Objects import obj_info
 
-from osbot_nginx.docker.nginx_in_lambda__mvp.Nginx_In_Lambda__MVP import Nginx_In_Lambda__MVP, ENV_VARS__REQUIRED, \
-    FOLDER__NGINX_IN_LAMBDA__MVP__SOURCE_FILES
+from osbot_nginx.docker.nginx_in_lambda__mvp.Nginx_In_Lambda__MVP import Nginx_In_Lambda__MVP, ENV_VARS__REQUIRED
 
 
 class test_Nginx_In_Lambda__MVP(TestCase):
@@ -21,8 +21,14 @@ class test_Nginx_In_Lambda__MVP(TestCase):
         for env_var_name in ENV_VARS__REQUIRED:
             assert getenv(env_var_name) is not None , f'Env var was not configured: {env_var_name}'
 
+    def test_create_image_ecr(self):
+        with self.nginx_in_lambda as _:
+            create_image_ecr = _.create_image_ecr()
+            assert create_image_ecr.path_image() == _.path_source_files()
+            assert create_image_ecr.image_name   == _.repository_name
+
     def test_ecr_container(self):
-        ecr_container = self.nginx_in_lambda.ecr_container()
+        ecr_container = self.nginx_in_lambda.ecr_repository()
 
         assert list_set(ecr_container) == ['createdAt', 'encryptionConfiguration', 'imageScanningConfiguration',
                                            'imageTagMutability', 'registryId', 'repositoryArn', 'repositoryName',
@@ -38,9 +44,8 @@ class test_Nginx_In_Lambda__MVP(TestCase):
 
     def test_path_source_files(self):
         path_source_files = self.nginx_in_lambda.path_source_files()
-        files_in_parh     = files_list(path_source_files, pattern='*')
         assert folder_exists(path_source_files)
-        assert folder_name  (path_source_files) == FOLDER__NGINX_IN_LAMBDA__MVP__SOURCE_FILES
+        assert folder_name  (path_source_files) == self.nginx_in_lambda.repository_name
         assert 'Dockerfile' in files_names(files_list(path_source_files, pattern='*'))
 
 
@@ -49,6 +54,6 @@ class test_Nginx_In_Lambda__MVP(TestCase):
             setup_data       = _.setup()
             target_repo_info = setup_data.get('target_repo_info')
             assert list_set(setup_data) == ['target_repo_info']
-            assert target_repo_info.get('repositoryName') == _.container_name
+            assert target_repo_info.get('repositoryName') == _.repository_name
 
-            assert target_repo_info.get('repositoryArn' ) == _.ecr_container_arn()
+            assert target_repo_info.get('repositoryArn' ) == _.ecr_repository_arn()
