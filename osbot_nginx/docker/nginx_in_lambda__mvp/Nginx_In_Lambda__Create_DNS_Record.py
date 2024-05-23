de2afdae83a88a0e6088f7b197bb6f3c965a6f59
Dinis Cruz <dinis.cruz@owasp.org>
2024-05-23 02:30:56 +0100

    added new tests to Create_Dns_Records

--- osbot_nginx/docker/nginx_in_lambda__mvp/Nginx_In_Lambda__Create_DNS_Record.py
+++ osbot_nginx/docker/nginx_in_lambda__mvp/Nginx_In_Lambda__Create_DNS_Record.py
@@ -3,6 +3,7 @@ from os import getenv
 from osbot_aws.AWS_Config import AWS_Config
 
 from osbot_aws.aws.route_53.Route_53 import Route_53
+from osbot_aws.aws.route_53.Route_53__Hosted_Zone import Route_53__Hosted_Zone
 from osbot_utils.base_classes.Kwargs_To_Self import Kwargs_To_Self
 
 from osbot_utils.decorators.methods.cache_on_self import cache_on_self
@@ -12,9 +13,11 @@ ENV_VAR_NAME__ROUTE_53__HOST_ZONE_ID = "ROUTE_53__HOST_ZONE_ID"
 class Nginx_In_Lambda__Create_DNS_Record(Kwargs_To_Self):
     aws_config     : AWS_Config
 
+
     @cache_on_self
-    def route_53(self):
-        return Route_53()
+    def hosted_zone(self):
+        hosted_zone_id = self.hosted_zone_id()
+        return Route_53__Hosted_Zone(hosted_zone_id=hosted_zone_id)
 
     def hosted_zone_id(self):
         return getenv(ENV_VAR_NAME__ROUTE_53__HOST_ZONE_ID)
--- tests/integration/docker/nginx_in_lambda__mvp/test_Nginx_In_Lambda__Create_DNS_Record.py
+++ tests/integration/docker/nginx_in_lambda__mvp/test_Nginx_In_Lambda__Create_DNS_Record.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+from osbot_aws.aws.route_53.Route_53__Hosted_Zone import Route_53__Hosted_Zone
 from osbot_utils.utils.Misc import list_set
 
 from osbot_utils.utils.Dev import pprint
@@ -10,10 +11,12 @@ from osbot_nginx.docker.nginx_in_lambda__mvp.Nginx_In_Lambda__Create_DNS_Record 
 
 class test_Nginx_In_Lambda__Create_DNS_Record(TestCase):
     create_dns_record : Nginx_In_Lambda__Create_DNS_Record
+    hosted_zone       : Route_53__Hosted_Zone
 
     @classmethod
     def setUpClass(cls):
         cls.create_dns_record = Nginx_In_Lambda__Create_DNS_Record()
+        cls.hosted_zone       = cls.create_dns_record.hosted_zone()
 
     # def test_route_53(self):
     #     with self.create_dns_record as _:
@@ -22,9 +25,17 @@ class test_Nginx_In_Lambda__Create_DNS_Record(TestCase):
     #         result = _.route_53().record_sets(hosted_zone_id = hosted_zone_id)
     #         pprint(result)
 
+    def test_a_records(self):
+        with self.hosted_zone as _:
+            assert 'nginx-amd64.dev.aws.cyber-boardroom.com.' in _.a_records()
+
+    def test_hosted_zone(self):
+        with self.hosted_zone as _:
+            assert type(_)            == Route_53__Hosted_Zone
+            assert list_set(_.info()) == ['CallerReference', 'Config', 'Id', 'Name', 'ResourceRecordSetCount']
+
+            record_sets = _.record_sets()
+            assert len(record_sets) > 0
+
     def test_hosted_zone_id(self):
         assert self.create_dns_record.hosted_zone_id() is not None
-        with self.create_dns_record as _:
-            hosted_zones = _.route_53().hosted_zones()
-            assert len(hosted_zones) == 1
-            assert list_set(hosted_zones[0]) == ['CallerReference', 'Config', 'Id', 'Name', 'ResourceRecordSetCount']
